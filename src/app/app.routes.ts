25d4bfa7a17ec4a5bd1786037e99ac8ec52aa791
Victor-Diniz-Dev <victordinizdev@gmail.com>
2025-06-16 16:37:24 -0300

    fix para visualizar no vercel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DishListComponent } from './components/dish-list/dish-list.component';
+import { DishFormComponent } from './components/dish-form/dish-form.component';
+import { CozinhaComponent } from './components/cozinha/cozinha.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',            redirectTo: 'dishes', pathMatch: 'full' },
+  { path: 'dishes',      component: DishListComponent },
+  { path: 'dishes/new',  component: DishFormComponent },
+  { path: 'dishes/:id/edit', component: DishFormComponent },
+  { path: 'orders/kitchen', component: CozinhaComponent }
+];
